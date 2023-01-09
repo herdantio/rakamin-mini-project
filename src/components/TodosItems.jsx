0aebce3ec63553417bfe63b1cd8eb86fc5e3e96a
HERDANTIO <herdantio.sindhunata@binus.ac.id>
2023-01-09 18:47:10 +0700

    feat: add new task button

--- src/components/TodosItems.jsx
+++ src/components/TodosItems.jsx
@@ -1,11 +1,16 @@
 import AppLabel from "./AppLabel"
+import PlusIcon from '../assets/Vector.svg';
 
 export default function TodosItems(props) {
     const {item} = props
     return (
-        <div className="p-4 space-y-2 bg-rk-green/[0.1]">
+        <div className="p-4 space-y-2 bg-rk-green/[0.1] w-[326px] rounded">
             <AppLabel text={item.title}/>
             <p className="text-xs font-bold">{item.description}</p>
+            <button className="text-xs flex flex-row space-x-2">
+                <img src={PlusIcon} />
+                <span>New Task</span>
+                </button>
         </div>
     )
 }
